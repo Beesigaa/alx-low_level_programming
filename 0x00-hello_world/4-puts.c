cc42220cabd3d418d5c1b04fd208ae7b2cf8c122
Adejoh <adejohadamah@gmail.com>
2022-10-13 21:18:48 -0700

    c programming is fun

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,18 +1,11 @@
 #include <stdio.h>
 /**
-
- *  *main - start of the code
- *
- *   *Return: value should be zero
- *
- *    *
- *
- *    	*we use put instead of print
- *
+*main - start of the code
+*Return: value should be zero
+*we use put instead of print
 */
 int main(void)
-
 {
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
