eab37ef759717067127b3f751e2f7ca99862635c
Adejoh <adejohadamah@gmail.com>
2022-10-25 14:46:10 -0700

    pointer task 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+  *reset_to_98 - a function that takes pointer to an int as pa	*rameter and updates the value it points to to 98
+  *@n: variable
+  *return:n 
+  */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
