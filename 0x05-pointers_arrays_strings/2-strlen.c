c7b1ce56a7e59715c34f42d3eeb99b6e6097e823
Adejoh <adejohadamah@gmail.com>
2022-10-26 00:01:43 -0700

    pointer task

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -2,18 +2,18 @@
 /**
 *_strlen- Function to return length of string
 *@s:string
-*@longi: string counter
+*@ta: string counter
 *Return: length
 */
 int _strlen(char *s)
 {
-	int longi = 0; /*counter*/
+	int ta = 0; /*counter*/
 
 	while (*s != '\0')
 	{
-		longi++;
+		ta++;
 		s++;
 	}
 
-	return (longi);
+	return (ta);
 }
