926eeeceb8bfd047db1a124f3b7170485c62bf2c
Adejoh <adejohadamah@gmail.com>
2022-10-27 13:20:55 -0700

    clean

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,10 +1,9 @@
 #include "main.h"
-
-/* *   * print_line - Draws a straight line according to parameter
+/**
+ * print_line - Draws a straight line according to parameter
  * @n: The number of lines to draw
  * Return: empty
 */
-
 void print_line(int n)
 {
 	int x;
