39dd4a8cde760cb48430ce6ba6d7c695079479d4
Adj <adejohadamah@gmail.com>
2023-07-29 15:57:11 -0700

    functions

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * jack_bauer - Prints the time count of the day number
+ * Return: Returns void
+ */
+void jack_bauer(void)
+{
+	int hr, mins;
+
+    for (hr = 0; hr < 24; hr++)
+    {
+	    for (mins = 0; mins < 60; mins++)
+	    {
+            _putchar(hr + ":" + mins);
+	    }
+    }
+}
