936c3bd65682ee8ef5844c2f967e76ede5761423
Adj <adejohadamah@gmail.com>
2023-07-30 13:43:21 -0700

    debugging

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - tests positive_or_negative
+ * Return: 0
+ * test function that prints if integer is positive or negative
+ */
+int main(void)
+{
+	int i;
+
+	i = 98;
+        positive_or_negative(i);
+        return (0);
+}
