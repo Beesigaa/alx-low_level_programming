aaa00bd67ae70f4620326e7e6510c7dbfb6c108b
Adj <adejohadamah@gmail.com>
2023-07-30 02:57:46 -0700

    jack bauer

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -8,16 +8,16 @@ void jack_bauer(void)
 	int Hr;
 	int Mins;
 
-    for (Hr = 0; Hr < 24; Hr++)
-    {
-	    for (Mins = 0; Mins < 60; Mins++)
-	    {
-		    _putchar((Hr / 10) + '0');
-		    _putchar((Hr % 10) + '0');
-		    _putchar(':');
-		    _putchar((Mins / 10) + '0');
-		    _putchar((Mins % 10) + '0');
-		    _putchar('\n');
-	    }
-    }
+	for (Hr = 0; Hr < 24; Hr++)
+	{
+		for (Mins = 0; Mins < 60; Mins++)
+		{
+			_putchar((Hr / 10) + '0');
+			_putchar((Hr % 10) + '0');
+			_putchar(':');
+			_putchar((Mins / 10) + '0');
+			_putchar((Mins % 10) + '0');
+			_putchar('\n');
+		}
+	}
 }
