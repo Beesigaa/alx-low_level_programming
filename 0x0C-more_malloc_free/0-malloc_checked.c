597aa812c60e0fcbe3b262f5d2404b3ef858f299
Adj <adejohadamah@gmail.com>
2023-08-11 16:39:52 -0700

    more malloc free

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * malloc_checked - allocate memory using malloc
+ * @b: number of bytes to allocate
+ * Return: a pointer to the allocated memory
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *pt;
+
+	pt = malloc(b);
+	if (pt == NULL)
+	{
+		exit(98);
+	}
+	return (pt);
+}
---
+++ 0x0C-more_malloc_free/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+*reset_to_98-Function that takes pointer to an int
+*as para*meter and updates the value it points to to 98
+*@n:variable
+*return:n
+*/
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
