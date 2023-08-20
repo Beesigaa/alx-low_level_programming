4d21a48a51f5311e93204e5f18df724ef4995540
Adj <adejohadamah@gmail.com>
2023-08-20 15:51:03 -0700

    static lib

---
+++ 0x09-static_libraries/0-strcat.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * _strcat - concatenates two strings
+ * @dest: input parameter
+ * @src: input parameter
+ * Return: void
+ */
+char *_strcat(char *dest, char *src)
+{
+	int m = 0;
+	int n = 0;
+
+	while (dest[m] != '\0')
+		m++;
+	while (src[n] != '\0')
+	{
+		dest[m] = src[n];
+		m++;
+		n++;
+	}
+	dest[m] = '\0';
+	return (dest);
+}
