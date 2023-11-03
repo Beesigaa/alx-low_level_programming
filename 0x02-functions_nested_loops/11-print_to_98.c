56bf579331b500d5080550a94d18046c665aebab
Beesigaa <adejohadamah@gmail.com>
2023-11-03 06:29:30 -0700

    nested loop update

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -6,27 +6,32 @@
   */
 void print_to_98(int n)
 {
-	if (n >= 0)
+	if (n < 98)
 	{
-		if (n < 98)
+		for (; n < 99; n++)
 		{
-			for (; n < 99; n++)
-			{
-				if (n < 10)
-					_putchar(n + '0');
-				else
-				{
-					_putchar((n / 10) + '0');
-					_putchar((n % 10) + '0');
-				}
-			}
-		}
-		else
-		{
-			for (; n > 97; n--)
+			if (n < 10)
 			{
 				_putchar(n + '0');
 			}
+			else
+			{
+				_putchar((n / 10) + '0');
+				_putchar((n % 10) + '0');
+			}
 		}
+	}
+	else if 
+	{
+		for (; n > 98; n--)
+		{
+			_putchar((n / 10) + '0');
+			_putchar((n % 10) + '0');
+		}
+	}
+	else
+	{
+		_putchar((n / 10) + '0');
+		_putchar((n % 10) + '0');
 	}
 }
